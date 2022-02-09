04e042f4d4a76b499e756cd2d1445ff01ebc2aa7
EXPedro <pedroelisio555@gmail.com>
2022-02-09 17:00:24 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Frontend para ecommerce 💽
+
+Projeto que demonstra um frontend de um ecommerce de discos de vinil, desenvolvido em react. Orientação da especialista
+[Nathally Souza]().
+
+### Ferramentas utilizadas 🔧
+
+- Bootstrap
+- React
+- Material UI
+- JavaScript
+
+### Resultado 🤩
+
+Após instalar as dependências, pode-se iniciar o projeto na porta 3000, com os comandos  
+
+```bash
+yarn install
+yarn start
+```  
+
+<img src="public/images/vinil.png" width="1000">  
+
+😜 Projeto concluído em 09/02/2022.
+
+
---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Raul Seixas - Novo Aeon",
+      price: 300,
+      image: "./images/produtos/raul-seixas-novo-aeon.jpg",
+      id_categorys: 1,
+      name_categorys: "Rock Nacional",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Chico César - Estado de Poesia",
+      price: 350,
+      image: "./images/produtos/estado-de-poesia.jpg",
+      id_categorys: 1,
+      name_categorys: "Rock Nacional",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Odair José - O Filho de José e Maria",
+      price: 230,
+      image: "./images/produtos/odair.jpg",
+      id_categorys: 1,
+      name_categorys: "Rock Nacional",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "internacional",
+      fk_idcategorys: 2,
+      name_product: "Nightwish - Oceanborn",
+      price: 399.99,
+      image: "./images/produtos/nightwish-oceanborn.jpg",
+      id_categorys: 2,
+      name_categorys: "Internacionais",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "internacional",
+      fk_idcategorys: 2,
+      name_product: "Pixies - Doolittle",
+      price: 350.99,
+      image: "./images/produtos/pixies-doolittle.jpg",
+      id_categorys: 2,
+      name_categorys: "Internacionais",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "internacional",
+      fk_idcategorys: 2,
+      name_product: "Angèle",
+      price: 299.99,
+      image: "./images/produtos/angele-brol-la-suite.jpg",
+      id_categorys: 2,
+      name_categorys: "Internacionais",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "classica",
+      fk_idcategorys: 3,
+      name_product: "Beatles - Revolver",
+      price: 200,
+      image: "./images/produtos/beatles-revolver.jpg",
+      id_categorys: 3,
+      name_categorys: "Clássicos",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "classica",
+      fk_idcategorys: 3,
+      name_product: "Pink Floyd- The Wall",
+      price: 450,
+      image: "./images/produtos/pink-floyd-the-wall.jpg",
+      id_categorys: 3,
+      name_categorys: "Clássicos",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "mpb",
+      fk_idcategorys: 4,
+      name_product: "Gilberto Gil - Refazenda",
+      price: 350,
+      image: "./images/produtos/gil-refazenda.jpg",
+      id_categorys: 4,
+      name_categorys: "MPB",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "mpb",
+      fk_idcategorys: 4,
+      name_product: "Gonzaguinha - Luis Gonzaga Junior",
+      price: 399,
+      image: "./images/produtos/gonzaguinha-jr.jpg",
+      id_categorys: 4,
+      name_categorys: "MPB",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "punk",
+      fk_idcategorys: 5,
+      name_product: "Toy Dolls - Bare Faced Cheek",
+      price: 180,
+      image: "./images/produtos/toy-dolls.jpg",
+      id_categorys: 5,
+      name_categorys: "Punk Rock",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "punk",
+      fk_idcategorys: 5,
+      name_product: "The Clash - London Calling",
+      price: 189.99,
+      image: "./images/produtos/clash-london-calling.jpg",
+      id_categorys: 5,
+      name_categorys: "Punk Rock",
+      selected: false
+    },
+];
+
+export default productsList;
